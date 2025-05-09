787416bfdd510159f035cd9886f2ac4853d87b05
Kristina Maximova <maximovaki@gmail.com>
2025-05-09 22:49:58 +0300

    done "CRUD" for message

--- sender/urls.py
+++ sender/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from sender.apps import SenderConfig
 
-from .views import (AddresseeCreateView,
-                    AddresseeDetailView,
-                    AddresseeUpdateView,
-                    AddresseeDeleteView,
+from .views import (AddresseeCreateView, AddresseeDetailView, AddresseeUpdateView,
+                    AddresseeDeleteView, AddresseeListView,
+                    MessageCreateView, MessageListView, MessageUpdateView,
+                    MessageDetailView, MessageDeleteView,
 
                     home_view)
 
@@ -13,9 +13,17 @@ app_name = SenderConfig.name  # 'sender'
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('addressee/new', AddresseeCreateView.as_view(), name='addressee_create'),
+
+    path('addressee/list/', AddresseeListView.as_view(), name='addressees_list'),
+    path('addressee/new/', AddresseeCreateView.as_view(), name='addressee_create'),
     path('addressee/<int:pk>/', AddresseeDetailView.as_view(), name='addressee_detail'),
     path('addressee/update/<int:pk>/', AddresseeUpdateView.as_view(), name='addressee_update'),
     path('addressee/delete/<int:pk>/', AddresseeDeleteView.as_view(), name='addressee_delete'),
+
+    path('message/list/', MessageListView.as_view(), name='messages_list'),
+    path('message/new/',MessageCreateView.as_view(), name='message_create'),
+    path('message/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
+    path('message/update/<int:pk>/', MessageUpdateView.as_view(), name='message_update'),
+    path('message/delete/<int:pk>', MessageDeleteView.as_view(), name='message_delete'),
 
 ]
--- sender/views.py
+++ sender/views.py
@@ -1,3 +1,5 @@
+from tempfile import template
+
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -11,27 +13,66 @@ class AddresseeCreateView(CreateView):
     """ Отображение получателя почты"""
     model = Addressee
     fields = ['email', 'full_name', 'comment']
-    template_name = 'sender/addressee_form.html'
-    success_url = reverse_lazy('sender:home')
+    template_name = 'sender/addressee/addressee_form.html'
+    success_url = reverse_lazy('sender:addressees_list')
 
 
 class AddresseeUpdateView(UpdateView):
     model = Addressee
     fields = ['email', 'full_name', 'comment']
-    template_name = 'sender/addressee_form.html'
-    success_url = reverse_lazy('sender:home')
+    template_name = 'sender/addressee/addressee_form.html'
+    success_url = reverse_lazy('sender:addressees_list')
 
 
 class AddresseeDetailView(DetailView):
     model = Addressee
-    template_name = 'sender/addressee_detail.html'
+    template_name = 'sender/addressee/addressee_detail.html'
     context_object_name = 'addressee'
 
 
 class AddresseeDeleteView(DeleteView):
     model = Addressee
-    template_name = 'sender/addressee_confirm_delete.html'
-    success_url = reverse_lazy('sender:home')
+    template_name = 'sender/addressee/addressee_confirm_delete.html'
+    success_url = reverse_lazy('sender:addressees_list')
+
+
+class AddresseeListView(ListView):
+    model = Addressee
+    template_name = 'sender/addressee/addressees_list.html'
+    context_object_name = 'addressees'
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    fields = ['subject', 'text']
+    template_name = 'sender/message/message_form.html'
+    success_url = reverse_lazy('sender:messages_list')
+
+class MessageListView(ListView):
+    model = Message
+    template_name = 'sender/message/messages_list.html'
+    context_object_name = 'messages'
+
+class MessageDetailView(DetailView):
+    model = Message
+    template_name = 'sender/message/message_detail.html'
+    context_object_name = 'message'
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    fields = ['subject', 'text']
+    template_name = 'sender/message/message_form.html'
+    success_url = reverse_lazy('sender:messages_list')
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    template_name = 'sender/message/message_confirm_delete.html'
+    success_url = reverse_lazy('sender:messages_list')
+
+
+class MailingCreateView(CreateView):
+    model = Mailing
+    fields = []
 
 
 def home_view(request):
